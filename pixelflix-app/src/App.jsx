ea56da7a9f466caf09f35c77fe5c9ac84871c8f0
j-weatherwax <joeyw475@gmail.com>
2024-06-11 17:56:33 -0400

    Adds Navigation bar to pages

--- pixelflix-app/src/App.jsx
+++ pixelflix-app/src/App.jsx
@@ -3,6 +3,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
+import Bookmarks from "./pages/Bookmarks/Bookmarks";
+import Movies from "./pages/Movies/Movies";
+import TV from "./pages/TV/TV";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/Bookmarks" element={<Bookmarks />} />
+      <Route path="/Movies" element={<Movies />} />
+      <Route path="/TV" element={<TV />} />
     </Routes>
   );
 }
