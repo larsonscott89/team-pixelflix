8a1793f24136b8294ac97958237555aed7c42fd9
Christian Ocampo <cocampo1005@yahoo.com>
2024-06-12 19:36:11 -0400

    Added AuthContext to set private routes that require authorization to access.

--- pixelflix-app/src/App.jsx
+++ pixelflix-app/src/App.jsx
@@ -6,17 +6,59 @@ import Home from "./pages/Home/Home";
 import Bookmarks from "./pages/Bookmarks/Bookmarks";
 import Movies from "./pages/Movies/Movies";
 import TV from "./pages/TV/TV";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { currentUser } = useAuth();
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Home />
+          </RequireAuth>
+        }
+      />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/Bookmarks" element={<Bookmarks />} />
-      <Route path="/Movies" element={<Movies />} />
-      <Route path="/TV" element={<TV />} />
+      <Route
+        path="/home"
+        element={
+          <RequireAuth>
+            <Home />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/Bookmarks"
+        element={
+          <RequireAuth>
+            <Bookmarks />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/Movies"
+        element={
+          <RequireAuth>
+            <Movies />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/TV"
+        element={
+          <RequireAuth>
+            <TV />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
