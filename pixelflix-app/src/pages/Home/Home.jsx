4fdb681e25a36f1d2aadd70aa9d599b626296091
Christian Ocampo <cocampo1005@yahoo.com>
2024-06-25 00:13:30 -0400

    Moved data fetch to Home component and created ProfileContext.

--- pixelflix-app/src/pages/Home/Home.jsx
+++ pixelflix-app/src/pages/Home/Home.jsx
@@ -14,29 +14,12 @@ import VideoList from "../../components/VideoList/VideoList";
 import TrendingList from "../../components/TrendingList/TrendingList";
 
 export default function Home() {
-  return (
-    <div className="base">
-      <Navbar />
-      <Searchbar />
-      <div className="base__content">
-        <Routes>
-          <Route path="/" element={<DefaultContent />} />
-          <Route path="/home" element={<DefaultContent />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/tv" element={<TV />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </div>
-    </div>
-  );
-}
-
-function DefaultContent() {
   const [videos, setVideos] = useState([]);
   const [trendingVideos, setTrendingVideos] = useState([]);
+  const [moviesList, setMoviesList] = useState([]);
+  const [showsList, setShowsList] = useState([]);
   const videosCollectionRef = collection(db, "Movies-TV");
+
   useEffect(() => {
     const getVideos = async () => {
       const data = await getDocs(videosCollectionRef);
@@ -45,13 +28,55 @@ function DefaultContent() {
         id: doc.id,
       }));
       setVideos(fetchedVideos);
+
       const trending = fetchedVideos.filter(
         (video) => video.isTrending === true
       );
       setTrendingVideos(trending);
+
+      const movies = fetchedVideos.filter(
+        (video) => video.category === "Movie"
+      );
+      setMoviesList(movies);
+
+      const shows = fetchedVideos.filter(
+        (video) => video.category === "TV Series"
+      );
+      setShowsList(shows);
     };
     getVideos();
   }, []);
+
+  return (
+    <div className="base">
+      <Navbar />
+      <Searchbar />
+      <div className="base__content">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DefaultContent videos={videos} trendingVideos={trendingVideos} />
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <DefaultContent videos={videos} trendingVideos={trendingVideos} />
+            }
+          />
+          <Route path="/bookmarks" element={<Bookmarks videos={videos} />} />
+          <Route path="/movies" element={<Movies videos={moviesList} />} />
+          <Route path="/tv" element={<TV videos={showsList} />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+function DefaultContent({ videos, trendingVideos }) {
   return (
     <div className="home">
       <h2 className="home__heading">Trending</h2>
