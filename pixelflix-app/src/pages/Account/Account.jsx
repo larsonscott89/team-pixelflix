13842f5b8ba28a9540e8721091d96a296d2a6531
j-weatherwax <joeyw475@gmail.com>
2024-10-26 19:22:15 -0400

    Allow User to Update Password

--- pixelflix-app/src/pages/Account/Account.jsx
+++ pixelflix-app/src/pages/Account/Account.jsx
@@ -1,27 +1,35 @@
 import "./Account.scss"
 import { FaEnvelope, FaLock, FaCreditCard, FaSignOutAlt, FaTrash } from 'react-icons/fa'
 import ThinChevronRight from '../../components/ThinChevronRight/ThinChevronRight'
-import { signOut } from "firebase/auth"
-import React, { useState, useEffect } from "react"
-import { doc, getDoc } from "firebase/firestore"
+import { EmailAuthProvider, reauthenticateWithCredential, signOut, updatePassword } from "firebase/auth"
+import React, { useState, useEffect, useRef } from "react"
 import { useNavigate } from "react-router-dom"
-import { auth, db } from "../../firebase-config"
+import { auth } from "../../firebase-config"
+
+import { useAuth } from "../../context/AuthContext"
 
 export default function Account() {
-  const navigate = useNavigate()
-  const [userEmail, setUserEmail] = useState("")
+  const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [updateMessage, setUpdateMessage] = useState("");
+  const passwordInputRef = useRef(null);
+
+  const [currentPasswordEmpty, setCurrentPasswordEmpty] = useState(false);
+  const [newPasswordEmpty, setNewPasswordEmpty] = useState(false);
+  const [newPasswordWeak, setNewPasswordWeak] = useState(false);
+  const [confirmPasswordEmpty, setConfirmPasswordEmpty] = useState(false);
+  const [confirmPasswordNotMatch, setConfirmPasswordNotMatch] = useState(false);
+
+  const { currentUser } = useAuth();
 
   useEffect(() => {
     const fetchUserEmail = async () => {
-      const user = auth.currentUser
-      if (user) {
-        const userDocRef = doc(db, "users", user.uid)
-        const userDoc = await getDoc(userDocRef)
-        if (userDoc.exists()) {
-          setUserEmail(user.email)
-        } else {
-          console.log("No such document!")
-        }
+      if (currentUser) {
+        setUserEmail(currentUser.email)
       } else {
         console.log("No user is signed in.")
       }
@@ -29,8 +37,8 @@ export default function Account() {
 
     fetchUserEmail()
 
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      if (currentUser) {
         fetchUserEmail()
       } else {
         setUserEmail("")
@@ -50,6 +58,115 @@ export default function Account() {
     }
   }
 
+  const validatePassword = (password) => {
+    // This regex tests for 1 capital letter, 1 lowercase letter, a number, a symbol, and a length of at least 8 characters
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+  
+  const handlePasswordChange = (e) => {
+    setNewPassword(e.target.value);
+    setNewPasswordEmpty(false);
+    setNewPasswordWeak(false);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordEmpty(false);
+    setConfirmPasswordNotMatch(false);
+  };
+  
+  const handleUpdatePassword = async (e) => {
+    e.preventDefault();
+
+    if (auth.currentUser) {
+      if (currentPassword === "") {
+        setCurrentPasswordEmpty(true);
+        return;
+      } else if (newPassword === "") {
+        setNewPasswordEmpty(true);
+        return;
+      } else if (!validatePassword(newPassword)) {
+        setNewPasswordWeak(true);
+        alert(
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol, and be at least 8 characters long."
+        );
+        return;
+      } else if (confirmPassword === "") {
+        setConfirmPasswordEmpty(true);
+        return;
+      } else if (newPassword !== confirmPassword) {
+        setConfirmPasswordNotMatch(true);
+        return;
+      }
+      
+      try {
+        const credential = EmailAuthProvider.credential(
+          auth.currentUser.email,
+          currentPassword
+        );
+
+        await reauthenticateWithCredential(auth.currentUser, credential);
+
+        await updatePassword(auth.currentUser, newPassword);
+        setUpdateMessage("Password updated successfully.");
+        setCurrentPassword("");
+        setNewPassword("");
+        setConfirmPassword("");
+      } catch (error) {
+        if (error.code === "auth/wrong-password") {
+          alert("Old password is incorrect. Please try again.");
+        } else {
+          setUpdateMessage("Error updating password: " + error.message);
+          console.error("Error updating password:", error);
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (!passwordInputRef.current) return;
+      const focusableElements = passwordInputRef.current.querySelectorAll('input, button, [tabindex]:not([tabindex="-1"])');
+      
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+
+      if (event.key === "Tab") {
+        if (event.shiftKey) {
+          if (document.activeElement === firstElement) {
+            event.preventDefault();
+            lastElement.focus();
+          }
+        } else {
+          if (document.activeElement === lastElement) {
+            event.preventDefault();
+            firstElement.focus();
+          }
+        }
+      }
+
+      if (event.key === "Escape") {
+        setShowPasswordModal(false);
+      }
+    };
+
+    if (showPasswordModal) {
+      passwordInputRef.current.focus();
+      document.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [showPasswordModal]);
+
+  const closeModal = () => {
+    setShowPasswordModal(false);
+    setUpdateMessage("");
+};
+
   return (
     <div>
       <h1 className="account__heading"> Account </h1>
@@ -58,16 +175,17 @@ export default function Account() {
           <button
             className="account__info-row email-box" 
             aria-label="email-box"
-            aria-role="button"
+            role="button"
           >
             <FaEnvelope /> 
             <span>Email</span>
             <p>{userEmail}</p>
           </button>
           <button 
-            className="account__info-row password-btn" 
+            className="account__info-row password-btn"
+            onClick={() => setShowPasswordModal(true)}
             aria-label="Update password"
-            aria-role="button"
+            role="button"
           >
             <FaLock /> 
             <span>Update Password</span>
@@ -78,7 +196,7 @@ export default function Account() {
           <button 
             className="account__info-row manage-btn"
             aria-label="Manage subscription"
-            aria-role="button"
+            role="button"
           >
             <FaCreditCard /> 
             <span>Manage Subscription</span>
@@ -90,7 +208,7 @@ export default function Account() {
             className="account__info-row signout-btn" 
             onClick={handleSignOut}
             aria-label="Sign out"
-            aria-role="button"
+            role="button"
           >
             <FaSignOutAlt />
             <span>Sign Out</span>
@@ -101,13 +219,91 @@ export default function Account() {
           <button 
             className="delete-btn"
             aria-label="Delete Account" 
-            aria-role="button"
+            role="button"
           >
             <FaTrash />
             <span>Delete Account</span>
           </button>
         </div>
       </div>
+
+      {showPasswordModal && (
+        <div className="modal" role="dialog" aria-labelledby="modal-title">
+          <div className="modal-content">
+            <h2 id="modal-title">Update Password</h2>
+            <form onSubmit={handleUpdatePassword}>
+              <div className="modal__form-inputdiv">
+              <div className={`modal__form-input-container${currentPasswordEmpty ? "--error" : ""}`}>
+                <input 
+                  type="password"
+                  name="currentPassword"
+                  id="currentPassword"
+                  placeholder="Enter your current password"
+                  onChange={(e) => {
+                    setCurrentPassword(e.target.value);
+                    setCurrentPasswordEmpty(false);
+                  }}
+                  aria-invalid={currentPasswordEmpty ? "true" : "false"}
+                  aria-describedby="currentPassword-error"
+                />
+                {currentPasswordEmpty && (
+                  <p id="currentPassword-error" className="modal__form-input--error" role="alert">Can't be empty</p>
+                )}
+              </div>
+
+                <div className={`modal__form-input-container${newPasswordEmpty || newPasswordWeak ? "--error" : ""}`}>
+                  <input 
+                    type="password"
+                    name="newPassword"
+                    id="newPassword"
+                    ref={passwordInputRef}
+                    onChange={handlePasswordChange}
+                    placeholder="Enter new password"
+                    aria-invalid={newPasswordEmpty || newPasswordWeak ? "true" : "false"}
+                    aria-describedby="newPassword-error"
+                  />
+                  {newPasswordEmpty && (
+                    <p id="newPassword-error" className="modal__form-input--error" role="alert">Can't be empty</p>
+                  )}
+                  {newPasswordWeak && (
+                    <p id="newPassword-error" className="modal__form-input--error" role="alert">Too weak</p>
+                  )}
+                </div>
+              
+                <div className={`modal__form-input-container${confirmPasswordEmpty || confirmPasswordNotMatch ? "--error" : ""}`}>
+                  <input 
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    placeholder="Confirm new password"
+                    onChange={handleConfirmPasswordChange}
+                    aria-invalid={confirmPasswordEmpty || confirmPasswordNotMatch ? "true" : "false"}
+                    aria-describedby="confirmPassword-error"
+                  />
+                  {confirmPasswordEmpty && (
+                    <p id="confirmPassword-error" className="modal__form-input--error" role="alert">Can't be empty</p>
+                  )}
+                  {confirmPasswordNotMatch && (
+                    <p id="confirmPassword-error" className="modal__form-input--error" role="alert">
+                      Passwords don't match
+                    </p>
+                  )}
+                </div>
+              </div>
+              
+              {updateMessage && <p className="update-message">{updateMessage}</p>}
+              
+              <div className="button-container">
+                <button type="submit" className="Update-Password__button">
+                  Update
+                </button>
+                <button onClick={closeModal}>Cancel</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }
